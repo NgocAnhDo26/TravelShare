98c5e8218f0fe3f5c49c25fd916f3fe14df14682
Ho Phuoc Hoan <phuochoan17032004@gmail.com>
2025-06-06 12:28:09 +0700

    revert to original file jest.config.js

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,3 +1,6 @@
-export const preset = 'ts-jest';
-export const testEnvironment = 'node';
-export const testMatch = ['**/tests/**/*.test.ts'];
+// eslint-disable-next-line no-undef
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+};
