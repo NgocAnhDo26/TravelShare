4b9998852bb07095d0c45db628ecb7e13455a2c5
NgocAnhDo26 <dotanngocanh@gmail.com>
2025-06-13 23:51:36 +0700

    refactor: replace hardcoded API URL with environment variable (for CI/CD)

--- client/src/utils/axiosInstance.tsx
+++ client/src/utils/axiosInstance.tsx
@@ -1,9 +1,39 @@
-import axios from 'axios';
+import axios from "axios";
+
+type EnvMode = "local" | "dev" | "prod";
+
+const env = process.env.REACT_APP_ENV as EnvMode | undefined;
+let baseURL: string;
+
+switch (env) {
+    case "local":
+        baseURL = "http://localhost:3000/api";
+        break;
+    case "dev":
+        if (!process.env.API_URL_DEV) {
+            throw new Error(
+                "REACT_APP_API_URL is not defined for production mode."
+            );
+        }
+        baseURL = process.env.API_URL_DEV;
+        break;
+    case "prod":
+        if (!process.env.API_URL_PROD) {
+            throw new Error(
+                "REACT_APP_API_URL is not defined for production mode."
+            );
+        }
+        baseURL = process.env.API_URL_PROD;
+        break;
+    default:
+        console.warn("Unknown REACT_APP_ENV. Defaulting to local mode.");
+        baseURL = "http://localhost:3000/api";
+}
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api',
-  // timeout: 10000, // 10 seconds timeout
-  withCredentials: true, // Include credentials for Cookies
+    baseURL,
+    // timeout: 10000, // 10 seconds timeout
+    withCredentials: true, // Include credentials for Cookies
 });
 
 export default API;
