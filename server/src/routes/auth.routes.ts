fa0d13a493807855c3bc26170d25aaaad409729f
Ho Phuoc Hoan <phuochoan17032004@gmail.com>
2025-06-19 17:06:43 +0700

    Bug: Email service now works - Updated SMTP_HOST to use the correct environment variable for email service configuration. - Update npm packages

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -5,8 +5,7 @@ import uploadUseCases from '../middlewares/upload';
 const authRouter: Router = Router();
 authRouter.post(
   '/register',
-  uploadUseCases.uploadMiddleware.single('avatar'),
-  uploadUseCases.uploadToSupabase, // Middleware to handle file upload and upload to Supabase
+  uploadUseCases.singleFileUpload('avatar'),
   authController.register,
 );
 
