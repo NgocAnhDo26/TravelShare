98919875ff371be1d7d08bbedda383acbd9b2881
Seapeas <dodinhhai29904@gmail.com>
2025-06-26 21:32:03 +0700

    feat: add itinerary management page with mock data and UI components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,48 +3,55 @@ import LoginPage from "./route/login/page";
 import ForgotPasswordPage from "./route/forgot-password/page";
 import ResetPasswordPage from "./route/reset-password/page";
 import UserProfilePage from "./route/UserProfilePage/page";
-import OtherProfilePage from "./route/OtherProfilePage/page"
+import OtherProfilePage from "./route/OtherProfilePage/page";
 import TripPlanningPage from "./route/trip-planning/page";
-import Header from "./components/header";
+import ItineraryPage from "./route/itinerary/page";
 import AddItineraryPage from "./route/add-itinerary/page";
 import { Toaster } from "react-hot-toast";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
-    return (
-        <>
-            <div>
-                <Toaster />
-            </div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/register" element={<RegisterPage />} />
-                    <Route
-                        path="/forgot-password"
-                        element={<ForgotPasswordPage />}
-                    />
-                    <Route
-                        path="/reset-password"
-                        element={<ResetPasswordPage />}
-                    ></Route>
-                    <Route
-                        path="/UserProfilePage"
-                        element={<UserProfilePage />}
-                    ></Route>
-                    <Route
-                        path="/OtherProfilePage"
-                        element={<OtherProfilePage />}
-                    ></Route>
-                    <Route path="/" element={<LoginPage />} />
-                    <Route path="/plan/create" element={<TripPlanningPage />} />
-                    <Route path="/test" element={<AddItineraryPage></AddItineraryPage>} /> 
-                    {/* Test route for components */}
-                    <Route path="*" element={<div>404 Not Found</div>} />
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
+	return (
+		<>
+			<div>
+				<Toaster />
+			</div>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/register" element={<RegisterPage />} />
+					<Route
+						path="/forgot-password"
+						element={<ForgotPasswordPage />}
+					/>
+					<Route
+						path="/reset-password"
+						element={<ResetPasswordPage />}
+					></Route>
+					<Route
+						path="/UserProfilePage"
+						element={<UserProfilePage />}
+					></Route>
+					<Route
+						path="/OtherProfilePage"
+						element={<OtherProfilePage />}
+					></Route>
+					<Route path="/" element={<LoginPage />} />
+					<Route path="/plan/create" element={<TripPlanningPage />} />
+					<Route
+						path="/itinerary/add"
+						element={<AddItineraryPage></AddItineraryPage>}
+					></Route>
+					<Route
+						path="/test"
+						element={<ItineraryPage></ItineraryPage>}
+					/>
+					{/* Test route for components */}
+					<Route path="*" element={<div>404 Not Found</div>} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
